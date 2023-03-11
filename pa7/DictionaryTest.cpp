9e1e01c5f5ba1e7760270beb34c07763db6f5436
Anderson Joseph Compalas <acompala@unix4.lt.ucsc.edu>
2023-03-10 17:03:33 -0800

    pa7

---
+++ pa7/DictionaryTest.cpp
@@ -0,0 +1,223 @@
+/*
+----------------------------------------
+Class: CSE101
+Project: pa7
+Name: Anderson Compalas
+CRUZID: 1793470
+Cruz Email: acompala
+Filename: DictionaryTest.cpp
+----------------------------------------
+*/
+
+//-----------------------------------------------------------------------------
+// DictionaryClient.cpp
+// A test client for the Dictionary ADT
+//-----------------------------------------------------------------------------
+#include<iostream>
+#include<string>
+#include<stdexcept>
+#include"Dictionary.h"
+
+using namespace std;
+
+int main(){
+
+
+   string s;
+   int x;
+   string S[] =   {
+                     "plaices",
+                     "ambusher",
+                     "crosby",
+                     "wattles",
+                     "pardoner",
+                     "pythons",
+                     "second",
+                     "forms",
+                     "impales",
+                     "pic",
+                     "verticals",
+                     "recriminator",
+                     "depressants",
+                     "cul",
+                     "potables",
+                     "norm",
+                     "reinsurer",
+                     "deschooler",
+                     "recoining",
+                     "dissocialised",
+                     "cohabit",
+                     "hemiolia",
+                     "cantling",
+                     "glamorized",
+                     "millesimal",
+                     "glagolitic"
+                  };
+   string T[] =   {
+                     "second",
+                     "forms",
+                     "impales",
+                     "pic",
+                     "verticals",
+                     "recriminator",
+                     "depressants",
+                     "cul",
+                     "potables",
+                     "norm",
+                     "reinsurer",
+                     "deschooler",
+                     "recoining",      
+                  };
+
+   Dictionary A;
+   Dictionary B;
+
+   cout << endl;
+
+   // insert some pairs into A
+   for(int i=0; i<26; i++){
+      A.setValue(S[i], i+1);
+   }
+   
+   // call operator=()
+   B = A;
+
+   cout << "A.size() = " << A.size() << endl << A << endl;
+   cout << "B.size() = " << B.size() << endl  << B << endl;
+
+   B.setValue("deschooler", 101);
+   B.setValue("reinsurer", 102);
+   B.setValue("glagolitic", 103);
+   B.setValue("cul", 104);
+   B.setValue("ambusher", 105);
+
+   // call copy constructor
+   Dictionary C = B;
+
+   cout << "B.size() = " << B.size() << endl  << B << endl;
+   cout << "C.size() = " << C.size() << endl  << C << endl;
+
+   //string s1 = B.to_string();
+   //string s2 = C.to_string();
+
+   //cout << "s1==s2 is " << (s1==s2?"true":"false") << endl;
+   
+   // check operator==()
+   cout << "A==B is " << (A==B?"true":"false") << endl;
+   cout << "B==C is " << (B==C?"true":"false") << endl;
+   cout << "C==A is " << (C==A?"true":"false") << endl << endl;
+
+   // perform alterations on A
+   cout << A.getValue("hemiolia") << endl;
+   A.getValue("hemiolia") *= 10; // change the value associated with "hemiolia"
+   cout << A.getValue("hemiolia") << endl << endl;
+
+   // check state of A
+   cout << "A.size() = " << A.size() << endl  << A << endl;
+   cout << A.pre_string() << endl;
+
+   // remove some pairs from A
+   for(int i=0; i<13; i++){
+      cout << "removing " << T[i] << " from Dictionary A" << endl; 
+      A.remove(T[i]);
+   }
+
+   cout << "after remove loop" << endl;
+
+   // check state of A
+   cout << "A.size() = " << A.size() << endl  << A << endl;
+   cout << A.pre_string() << endl;
+
+   // do forward iteration on A
+   for(A.begin(); A.hasCurrent(); A.next()){
+      s = A.currentKey();
+      x = A.currentVal();
+      cout << "("+s+", " << x << ") ";
+   }
+   cout << endl << endl;
+
+   // do reverse iteration on A
+   for(A.end(); A.hasCurrent(); A.prev()){
+      s = A.currentKey();
+      x = A.currentVal();
+      cout << "("+s+", " << x << ") ";
+   }
+   cout << endl << endl;
+   
+   // check exceptions
+   try{
+      A.getValue("blah");
+   }catch(logic_error& e){
+      cout << e.what() << endl;
+      cout << "   continuing without interruption" << endl;
+   }
+   try{
+      A.remove("blah");
+   }catch(logic_error& e){
+      cout << e.what() << endl;
+      cout << "   continuing without interruption" << endl;
+   }
+   try{
+      A.currentKey();
+   }catch(logic_error& e){
+      cout << e.what() << endl;
+      cout << "   continuing without interruption" << endl;
+   }
+   try{
+      A.currentVal();
+   }catch(logic_error& e){
+      cout << e.what() << endl;
+      cout << "   continuing without interruption" << endl;
+   }  
+   try{
+      A.next();
+   }catch(logic_error& e){
+      cout << e.what() << endl;
+      cout << "   continuing without interruption" << endl;
+   }
+   try{
+      A.prev();
+   }catch(logic_error& e){
+      cout << e.what() << endl;
+      cout << "   continuing without interruption" << endl;
+   }
+
+   cout << endl; 
+   
+
+   Dictionary R;
+
+   R.setValue("d", 1);
+   R.setValue("b", 5);
+   R.setValue("a", 10);
+   R.setValue("c", 15);
+   R.setValue("f", 20);
+   R.setValue("e", 25);
+   R.setValue("g", 30);
+   R.begin();
+   cout << "Move to beginning" << endl;
+   cout << "Current key:val " << R.currentKey() << ":" << R.currentVal() << endl << endl;
+   cout << "Before delete a:" << endl << R << endl;
+   R.remove("a"); 
+   //cout << "Current key:val " << R.currentKey() << ":" << R.currentVal() << endl << endl;
+   R.end();
+   cout << "Move to end" << endl;
+   cout << "Current key:val " << R.currentKey() << ":" << R.currentVal() << endl << endl;
+   cout << "Before delete d:" << endl << R << endl;
+   R.remove("d");
+   cout << "After delete d:" << endl << R << endl;
+   //R.end();
+   cout << "Current key:val " << R.currentKey() << ":" << R.currentVal() << endl << endl;
+   cout << "Correct key:val " <<"g:30" << endl;
+
+   if (R.currentKey() != "g" || R.currentVal() != 30){
+      cout << "Test failed" << endl;
+   }else{
+      cout << "Test passed" << endl;
+   }
+   
+   
+   
+
+   return( EXIT_SUCCESS );
+}
